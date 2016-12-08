ec12da9b8f4e6d5ca8d20e9249fe498fa24704aa
KevinLaw <kevinlaw@thinkgeo.com>
2016-12-08 11:27:30 +0800

    Update the sample.

---
+++ WorldStreetsRenderLayer/OsmWorldMapKitCompositeStyle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using ThinkGeo.MapSuite.Drawing;
+using ThinkGeo.MapSuite.Shapes;
+using ThinkGeo.MapSuite.Styles;
+
+namespace ThinkGeo.MapSuite
+{
+    class OsmWorldMapKitCompositeStyle : CompositeStyle
+    {
+        protected override void DrawCore(IEnumerable<Feature> features, GeoCanvas canvas, Collection<SimpleCandidate> labelsInThisLayer, Collection<SimpleCandidate> labelsInAllLayers)
+        {
+            Collection<Feature> tempFeatures = new Collection<Feature>();
+
+            foreach (var feature in features)
+            {
+                tempFeatures.Add(feature);
+                base.DrawCore(tempFeatures, canvas, labelsInThisLayer, labelsInAllLayers);
+                tempFeatures.Remove(feature);
+            }
+        }
+    }
+}
